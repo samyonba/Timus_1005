9dbc50d7affa211e5c8e04db3bec103c38d99ce6
samyonba <samyonba@gmail.com>
2022-03-09 18:34:37 +0300

    complete

--- Timus_1005/Timus_1005.cpp
+++ Timus_1005/Timus_1005.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+#include <bitset>
 
 using namespace std;
 
@@ -15,13 +16,9 @@ int main()
 
     int total = 0;
     int left_sum = 0;
-    int right_sum = 0;
-
-    vector<int> left_part;
-    vector<int> right_part;
 
     vector<int> data;
-    data.reserve(n);
+    /*data.resize(20);*/
 
     for (size_t i = 0; i < n; i++)
     {
@@ -30,100 +27,33 @@ int main()
         data.push_back(value);
         total += value;
     }
-
-	sort(data.begin(), data.end(), [](int a, int b) {
-		return a > b;
-		});
-
-    int mid_value = total / 2;
-
-    size_t k = 0;
-    while (left_sum < mid_value)
-    {
-        left_part.push_back(data[k]);
-        left_sum += data[k];
-        k++;
-    }
-    for (size_t i = k; i < data.size(); i++)
-    {
-        right_part.push_back(data[i]);
-        right_sum += data[i];
-    }
-
-    //if (left_sum * 2 == total)
-
-    int min_difference = abs(left_sum - right_sum);
+    int min_difference = total;
 
     
 
-    for (size_t i = 0; i < right_part.size(); i++)
+    for (size_t i = 0; i < pow(2, n); i++)
     {
-        vector<int> add_left = left_part;
-        vector<int> add_right = right_part;
+        bitset<20> mask{ i };
+        
+        int local_sum = 0;
 
-        int add_left_sum = left_sum;
-        int add_right_sum = right_sum;
-
-
-        add_left_sum -= add_left.back();
-        add_left_sum += add_right[i];
-        add_right_sum -= add_right[i];
-        add_right_sum += add_left.back();
-
-        int buf = add_left.back();
-        add_left.back() = add_right[i];
-        add_right[i] = buf;
-
-        size_t j = right_part.size() - 1;
-        int inner_left_sum = add_left_sum;
-        while (inner_left_sum < mid_value && j >= 0 && j < add_right.size())
+        for (size_t j = 0; j < n; j++)
         {
-            vector<int> inner_left = add_left;
-            vector<int> inner_right = add_right;
-
-            inner_left_sum = add_left_sum;
-            int inner_right_sum = add_right_sum;
-
-            inner_left_sum += inner_right[j];
-            inner_right_sum -= inner_right[j];
-
-            if (abs(inner_left_sum - inner_right_sum) < min_difference)
+            if (mask[j])
             {
-                min_difference = abs(inner_left_sum - inner_right_sum);
+                local_sum += data[j];
             }
-
-            inner_left.push_back(inner_right[j]);
-            inner_right.erase(inner_right.end() - 1);
-
-            if (inner_left_sum > mid_value)
-            {
-                sort(inner_left.begin(), inner_left.end(), [](int a, int b) {
-                    return a > b;
-                    });
-
-                inner_left_sum -= inner_left.back();
-                inner_right_sum += inner_left.back();
-
-                if (abs(inner_left_sum - inner_right_sum) < min_difference)
-                {
-                    min_difference = abs(inner_left_sum - inner_right_sum);
-                }
-
-                inner_right.push_back(inner_left.back());
-                inner_left.erase(inner_left.end() - 1);
-                
-            }
-
-            j--;
         }
 
-        if (abs(add_left_sum - add_right_sum) < min_difference)
+        int local_difference = abs(total - 2 * local_sum);
+        if (local_difference < min_difference)
         {
-            min_difference = abs(add_left_sum - add_right_sum);
+            min_difference = local_difference;
         }
     }
 
     cout << min_difference << endl;
+	
     return 0;
 }
 
