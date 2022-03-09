bf5fa72de4071a267aa1947b3e0d1fe945982e96
samyonba <samyonba@gmail.com>
2022-03-09 17:22:05 +0300

    first_try (fail)

--- Timus_1005/Timus_1005.cpp
+++ Timus_1005/Timus_1005.cpp
@@ -13,8 +13,12 @@ int main()
     size_t n;
     cin >> n;
 
-    int sum_left = 0;
-    int sum_right = 0;
+    int total = 0;
+    int left_sum = 0;
+    int right_sum = 0;
+
+    vector<int> left_part;
+    vector<int> right_part;
 
     vector<int> data;
     data.reserve(n);
@@ -24,25 +28,102 @@ int main()
         int value;
         cin >> value;
         data.push_back(value);
+        total += value;
     }
 
 	sort(data.begin(), data.end(), [](int a, int b) {
 		return a > b;
 		});
 
-    for (auto value : data)
+    int mid_value = total / 2;
+
+    size_t k = 0;
+    while (left_sum < mid_value)
     {
-        if (abs(sum_left + value - sum_right) <= abs(sum_right + value - sum_left))
+        left_part.push_back(data[k]);
+        left_sum += data[k];
+        k++;
+    }
+    for (size_t i = k; i < data.size(); i++)
+    {
+        right_part.push_back(data[i]);
+        right_sum += data[i];
+    }
+
+    //if (left_sum * 2 == total)
+
+    int min_difference = abs(left_sum - right_sum);
+
+    
+
+    for (size_t i = 0; i < right_part.size(); i++)
+    {
+        vector<int> add_left = left_part;
+        vector<int> add_right = right_part;
+
+        int add_left_sum = left_sum;
+        int add_right_sum = right_sum;
+
+
+        add_left_sum -= add_left.back();
+        add_left_sum += add_right[i];
+        add_right_sum -= add_right[i];
+        add_right_sum += add_left.back();
+
+        int buf = add_left.back();
+        add_left.back() = add_right[i];
+        add_right[i] = buf;
+
+        size_t j = right_part.size() - 1;
+        int inner_left_sum = add_left_sum;
+        while (inner_left_sum < mid_value && j >= 0 && j < add_right.size())
         {
-            sum_left += value;
+            vector<int> inner_left = add_left;
+            vector<int> inner_right = add_right;
+
+            inner_left_sum = add_left_sum;
+            int inner_right_sum = add_right_sum;
+
+            inner_left_sum += inner_right[j];
+            inner_right_sum -= inner_right[j];
+
+            if (abs(inner_left_sum - inner_right_sum) < min_difference)
+            {
+                min_difference = abs(inner_left_sum - inner_right_sum);
+            }
+
+            inner_left.push_back(inner_right[j]);
+            inner_right.erase(inner_right.end() - 1);
+
+            if (inner_left_sum > mid_value)
+            {
+                sort(inner_left.begin(), inner_left.end(), [](int a, int b) {
+                    return a > b;
+                    });
+
+                inner_left_sum -= inner_left.back();
+                inner_right_sum += inner_left.back();
+
+                if (abs(inner_left_sum - inner_right_sum) < min_difference)
+                {
+                    min_difference = abs(inner_left_sum - inner_right_sum);
+                }
+
+                inner_right.push_back(inner_left.back());
+                inner_left.erase(inner_left.end() - 1);
+                
+            }
+
+            j--;
         }
-        else
+
+        if (abs(add_left_sum - add_right_sum) < min_difference)
         {
-            sum_right += value;
+            min_difference = abs(add_left_sum - add_right_sum);
         }
     }
 
-    cout << abs(sum_left - sum_right) << endl;
+    cout << min_difference << endl;
     return 0;
 }
 
